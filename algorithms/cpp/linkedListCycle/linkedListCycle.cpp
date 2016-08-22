af01be69630dabd6879d00f914b0dd184b2f6f2c
wliangatyahoo <dustbox@gmail.com>
2016-08-22 11:51:56 -0700

    Update linkedListCycle.cpp

--- algorithms/cpp/linkedListCycle/linkedListCycle.cpp
+++ algorithms/cpp/linkedListCycle/linkedListCycle.cpp
@@ -21,12 +21,17 @@
  */
 
 bool hasCycle(ListNode *head) {
-    if (head==NULL || head->next==NULL) return false;
-    ListNode* fast=head;
-    ListNode* slow=head;
-    do{
-        slow = slow->next;
-        fast = fast->next->next;
-    }while(fast != NULL && fast->next != NULL && fast != slow);
-    return fast == slow? true : false;
+	if (head == NULL || head->next == NULL) return false;
+
+	ListNode *fast = head, *slow = head;
+
+	while (fast != NULL && fast->next != NULL) {
+		fast = fast->next->next;
+		slow = slow->next;
+		if (fast == slow) {
+			return true;
+		}
+	}
+
+	return false;
 } 
