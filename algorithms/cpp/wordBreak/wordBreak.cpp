b79e9a1a3002fca04b9e2125d024a854ed891477
wliangatyahoo <dustbox@gmail.com>
2016-08-22 00:34:55 -0700

    Update wordBreak.cpp

--- algorithms/cpp/wordBreak/wordBreak.cpp
+++ algorithms/cpp/wordBreak/wordBreak.cpp
@@ -23,28 +23,20 @@ using namespace std;
 
 bool wordBreak(string s, set<string> &dict) {
 
-    //using an array to mark subarray from 0 to i can be broken or not
-    vector<bool> v(s.size(),false);
-
-    for(int i=0; i<s.size(); i++){
-        //check the substring from 0 to i is int dict or not
-        string w = s.substr(0,i+1);
-        v[i] = (dict.find(w)!=dict.end());
-
-        //if it is, then use greedy algorithm
-        if (v[i]) continue;
-
-        //if it is not, then break it to check
-        for(int j=0; j<i; j++){
-            //if the substring from 0 to j can be borken, then check the substring from j to i
-            if (v[j]==true){
-                w = s.substr(j+1, i-j);
-                v[i] = (dict.find(w)!=dict.end());
-                if (v[i]) break;
-            }
-        }
-    }
-    return v.size() ? v[v.size()-1] : false;
+	  vector<bool> match(s.size()+1, false);
+	  match[0] = true; //initial state so we can move forward
+	  for (int i = 0; i<s.size(); ++i)
+	  {
+	       if (match[i]) {
+	        for (const auto &x:dict) {
+	           int end = x.size() + i;
+	           if (end <= s.length() && (s.substr(i, x.size()) == x)) {
+                    match[end] = true;
+	          }
+	       }
+	    }
+	  }
+	  return match[s.size()];
 }
 
 
