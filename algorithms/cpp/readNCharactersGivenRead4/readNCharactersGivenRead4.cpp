c1c38bf498d25986db8922fed133da9e28f6ac5f
wliangatyahoo <dustbox@gmail.com>
2016-08-22 23:10:54 -0700

    Update readNCharactersGivenRead4.cpp

--- algorithms/cpp/readNCharactersGivenRead4/readNCharactersGivenRead4.cpp
+++ algorithms/cpp/readNCharactersGivenRead4/readNCharactersGivenRead4.cpp
@@ -29,43 +29,20 @@ class Solution {
          * @return    The number of characters read
          */
         int read(char *buf, int n) {
-            srand(time(0));
-            if (rand()%2){
-                return read1(buf, n);
-            }
-            return read2(buf, n);
+                    int len = 0;
+        int m = INT_MAX;
+        while(len+4<=n) {
+            m = read4(buf+len);
+            len += m;
+            if(m<4) break;
         }
+        if(len==n || m<4) return len;
 
-        //read the data in-place. potential out-of-boundary issue
-        int read1(char *buf, int n) {
-            int len = 0;
-            int size = 0;
-
-            // `buf` could be accessed out-of-boundary 
-            while(len <=n && (size = read4(buf))>0){
-                size = len + size > n ?  n - len : size;
-                len += size;
-                buf += size;
-            }
-            return  len;
-        }
-
-        //using a temp-buffer to avoid peotential out-of-boundary issue
-        int read2(char *buf, int n) {
-            char _buf[4];   // the buffer for read4()
-            int _n = 0;     // the return for read4()
-            int len = 0;    // total buffer read from read4()
-            int size = 0;   // how many bytes need be copied from `_buf` to `buf`
-            while((_n = read4(_buf)) > 0){
-                //check the space of `buf` whether full or not
-                size = len + _n > n ? n-len : _n;
-                strncpy(buf+len, _buf, size);
-                len += size;
-                //buffer is full
-                if (len>=n){
-                    break;
-                }
-            }
-            return len;
+        char *remain = new char[5];
+        m = min(read4(remain),n-len);
+        for(int i=0; i<m; i++) buf[len++] = remain[i];
+        delete remain;
+        
+        return len;
         }
 };
