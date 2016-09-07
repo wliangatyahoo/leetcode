19b5b6a10376dc36ec081bc820fc77de3fd44ff1
wliangatyahoo <dustbox@gmail.com>
2016-09-07 15:02:39 -0700

    Update constructBinaryTreeFromPreorderAndInorderTraversal.cpp

--- algorithms/cpp/constructBinaryTreeFromPreorderAndInorderTraversal/constructBinaryTreeFromPreorderAndInorderTraversal.cpp
+++ algorithms/cpp/constructBinaryTreeFromPreorderAndInorderTraversal/constructBinaryTreeFromPreorderAndInorderTraversal.cpp
@@ -24,51 +24,21 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-TreeNode *buildTree(vector<int>& preorder, int& preidx, vector<int>& inorder);
-
-TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder) {
-    int preidx=0;
-    return buildTree(preorder, preidx, inorder);
+typedef vector<int>::iterator Iter;
+TreeNode *buildTreeRecur(Iter istart, Iter iend, Iter pstart, Iter pend)
+{
+    if(istart == iend)return NULL;
+    int rootval = *pstart;
+    Iter iterroot = find(istart, iend, rootval);
+    TreeNode *res = new TreeNode(rootval);
+    res->left = buildTreeRecur(istart, iterroot, pstart+1, pstart+1+(iterroot-istart));
+    res->right = buildTreeRecur(iterroot+1, iend, pstart+1+(iterroot-istart), pend);
+    return res;
 }
 
-TreeNode *buildTree(vector<int>& preorder, int& preidx, vector<int>& inorder) {
-
-    if (preorder.size()<=0 || inorder.size()<=0 ) return NULL;
-
-    TreeNode *root = new TreeNode(preorder[preidx]);
-    if (inorder.size()==1){
-        return root;
-    }
-
-    int i;
-    for(i=0; i<inorder.size(); i++){
-        if (inorder[i] == preorder[preidx]){
-            break;
-        }
-    }
-
-    //error: not found
-    if (i == inorder.size()) return NULL;
-
-    if (preidx+1 >= preorder.size()){
-        return root;
-    }
-
-    
-    vector<int> v(inorder.begin(), inorder.begin()+i);
-    if (v.size()>0) {
-        preidx++;
-        root->left = buildTree(preorder, preidx, v);
-    }
-
-    v.clear();
-    v.assign(inorder.begin()+i+1, inorder.end());
-    if (v.size()>0) {
-        preidx++;
-        root->right = buildTree(preorder, preidx, v);
-    }
-
-    return root;
+TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder) {
+    // write your code here
+    return buildTreeRecur(inorder.begin(), inorder.end(), preorder.begin(), preorder.end());
 }
 
 void printTree_pre_order(TreeNode *root)
